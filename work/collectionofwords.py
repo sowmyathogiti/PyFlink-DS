10430d02b43635a932f1307709b760211d492bff
vigneshwar6666 <vigneshwar6666@gmail.com>
2021-02-18 22:51:53 -0600

    Added code file

---
+++ work/collectionofwords.py
@@ -0,0 +1,37 @@
+from pyflink.common.serialization import SimpleStringEncoder
+from pyflink.common.typeinfo import Types
+from pyflink.datastream.connectors import StreamingFileSink
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import StreamTableEnvironment, DataTypes
+from pyflink.table.descriptors import Schema, OldCsv, FileSystem
+from pyflink.table.udf import udf
+def collectionOfWords():
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(1)
+    t_env = StreamTableEnvironment.create(env)
+
+    add = udf(lambda i: len(i), [ DataTypes.STRING()], DataTypes.BIGINT())
+
+    t_env.register_function("add", add)
+
+    t_env.connect(FileSystem().path('/tmp/input')) \
+        .with_format(OldCsv() \
+                     .field('i', DataTypes.STRING()) \
+                     .with_schema(Schema() \
+                                  .field('i', DataTypes.STRING())) \
+                     .create_temporary_table('mySource'))
+
+    t_env.connect(FileSystem().path('/tmp/output')) \
+        .with_format(OldCsv()
+                     .field('rsl', DataTypes.STRING())) \
+        .with_schema(Schema()
+                     .field('rsl', DataTypes.STRING())) \
+        .create_temporary_table('mySink')
+
+    t_env.from_path('mySource') \
+        .select("add(i)") \
+        .insert_into('mySink')
+
+    t_env.execute("tutorial_job")
+if _name_ == '_main_':
+collectionOfWords()

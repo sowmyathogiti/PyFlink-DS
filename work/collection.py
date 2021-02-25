f9c75f69b653c89ad132ef144d04c69664741a14
sowmya thogiti <sowmyathogiti27@gmail.com>
2021-02-25 12:19:23 -0500

    updated code in collection of numbers code

--- work/collection.py
+++ work/collection.py
@@ -5,13 +5,15 @@ from pyflink.datastream import StreamExecutionEnvironment
 
 def collectionsOfNumbers():
     env = StreamExecutionEnvironment.get_execution_environment()
-    env.set_python_requirements("./input","./output")
+    # env.set_python_requirements("./input.txt","./output.txt")
     ds = env.from_collection(
-        collection=[[1], [2]],
-        type_info=Types.ROW([Types.INT()]))
-    ds.map((lambda x : x[0]*10))
+        collection=[0, 1, 2, 3],
+        type_info=Types.INT())
+    ds = ds.filter(lambda x : x % 2 == 0)
+    ds = ds.map(lambda x : x*10,Types.INT()).shuffle()
+
     ds.add_sink(StreamingFileSink
-                .for_row_format('./', SimpleStringEncoder())
+                .for_row_format('./collectionsOfNumbers.txt', SimpleStringEncoder())
                 .build())
     env.execute("tutorial_job")
 

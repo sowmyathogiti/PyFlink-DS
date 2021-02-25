83f55ed30b159f4c6e77caee386bec7db2bb1c0a
Rohitha12 <60019339+Rohitha12@users.noreply.github.com>
2021-02-25 14:23:16 -0600

    Updated code file

--- work/readandwrite.py
+++ work/readandwrite.py
@@ -10,19 +10,19 @@ def readAndWrite():
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_parallelism(1)
     t_env = StreamTableEnvironment.create(env)
-    words = ["wael", "hello"]
+    words = ["hi", "hello"]
 
     add = udf(lambda i: i  if any(word in i for word in words) else None, [ DataTypes.STRING()],DataTypes.STRING())
     t_env.register_function("add", add)
 
-    t_env.connect(FileSystem().path('/tmp/input')) \
+    t_env.connect(FileSystem().path('./input.txt')) \
         .with_format(OldCsv() \
-                     .field('i', DataTypes.STRING()) \
-                     .with_schema(Schema() \
-                                  .field('i', DataTypes.STRING())) \
-                     .create_temporary_table('mySource'))
+                     .field('i', DataTypes.STRING())) \
+        .with_schema(Schema() \
+                     .field('i', DataTypes.STRING())) \
+        .create_temporary_table('mySource')
 
-    t_env.connect(FileSystem().path('/tmp/output')) \
+    t_env.connect(FileSystem().path('./readAndWrite.txt')) \
         .with_format(OldCsv()
                      .field('rsl', DataTypes.STRING())) \
         .with_schema(Schema()
@@ -35,6 +35,5 @@ def readAndWrite():
 
     t_env.execute("tutorial_job")
 
-if _name_ == '_main_':
-
-readAndWrite()
+if __name__ == '__main__':
+    readAndWrite()
